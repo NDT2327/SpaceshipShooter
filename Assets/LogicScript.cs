e0ec1a60895192c042d7c72c957791c3e68def5f
Phạm Cường <phamcuongtran222@gmail.com>
2025-02-20 21:52:54 +0700

    add GameOverScene

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,15 +1,27 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text textScore;
+    public GameObject gameOverScreen;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         textScore.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
